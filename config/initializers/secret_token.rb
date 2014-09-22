cdedc414617d068e1857da33f4060430ff0091ce
Philip_Johnson <philip_johnson@live.com>
2014-09-21 21:19:25 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Applecider::Application.config.secret_token = '6d1effa89960353f8e1bd8e98e62685dd1e2c5ed38c7c925e2cce4b82ee986a1af7066176c841206e79e9218ede856bcfb18bc9df62eb8523e35568104d70aca'
